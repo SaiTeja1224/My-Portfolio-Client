8f164499dac0a6f871ff4d937798a020aa844a3a
SaiTeja <saiteja1224@proton.me>
2024-03-10 17:46:40 +0530

    fixed typescript issues

--- src/hooks/useActiveSectionOnScroll.tsx
+++ src/hooks/useActiveSectionOnScroll.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useActiveSection } from "../context/ActiveSectionContext";
 
 const useActiveSectionOnScroll = () => {
