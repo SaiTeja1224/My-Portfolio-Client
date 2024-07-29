27a9ab027d122a2c421259851048671aed86da37
SaiTeja1224 <saiteja1224@proton.me>
2024-07-29 11:28:25 +0530

    updated content for portfolio

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <article className="about" id="about">
-      <h2 className="section-heading">About me</h2>
+      <h2 className="section-heading">About Me</h2>
       <p>
         I am Sai Teja a passionate and creative developer based in Hyderabad,
         Telangana, India. As I studied <strong>computer science</strong>, I
@@ -9,15 +9,14 @@ const About = () => {
         curious about all the cool things you can do with tech. Out of many
         streams in programming <strong>web development</strong> appealed to me
         the most. It allows me paint my idea on the canvas and provide it for
-        the world to see. My core stack is <strong>React</strong>,{" "}
-        <strong>Node.js</strong>, and <strong>MongoDB</strong>. I am familliar
-        with TypeScript, Next.js, Python. I'm always eager to embrace new
+        the world to see. My core stack is <strong>Next.js</strong>, <strong>React</strong>,{" "}
+        <strong>Node.js</strong>, <strong>Typescript</strong>, and <strong>MongoDB</strong>. I'm always eager to embrace new
         technologies and expand my skill set. I'm looking forward to working
         with you.
       </p>
       <p>
         While I'm not coding, I enjoy watching movies and shows, drawing
-        pictures, playing video games and travelling.
+        pictures, playing video games and traveling.
       </p>
     </article>
   );
